3f5760f3ba5da91a3fe4e17f52bf1e850b23baa6
Mel Liu <privatemiao@gmail.com>
2015-09-26 21:45:58 +0800

    ~

--- toolbox/www/js/PhotoUploadService.js
+++ toolbox/www/js/PhotoUploadService.js
@@ -1,4 +1,4 @@
-angular.module('generic.services', []).factory('PhotoUploadService', function($q) {
+angular.module('generic.services', []).factory('PhotoUploadService', function($q, $timeout) {
 	return {
 		gatherPhotos : function(photos) {
 			var defer = $q.defer();
@@ -13,19 +13,51 @@ angular.module('generic.services', []).factory('PhotoUploadService', function($q
 			return defer.promise;
 		},
 		uploadPhotos : function(photos, progress) {
-			// resolveLocalFileSystemURL
+			var reference = this;
 			(function prepareUpload() {
-				console.log(photos[progress.currentIndex++]);
-				progress.name = progress.currentIndex;
-				if (photos.length > progress.currentIndex){
-					prepareUpload();
-				}else{
-					console.log(progress.currentIndex);
-				}
+				resolveLocalFileSystemURL(photos[progress.currentIndex++], function(entry) {
+					entry.file(function(file) {
+						var f = {
+							name : file.name,
+							lastModified : file.lastModified,
+							localURL : file.localURL,
+							size : file.size,
+							type : file.type
+						}
+						progress.type = (function() {
+							if (f.type.startsWith('image/')) {
+								return 'image';
+							} else {
+								return 'video';
+							}
+						})();
+						progress.name = f.name;
+						reference.uploadPhoto(f, progress).then(function() {
+							if (photos.length > progress.currentIndex) {
+								prepareUpload();
+							}
+						}, function(error) {
+							console.log('ERROR->', error)
+						});
+					});
+				});
 			})();
 		},
-		uploadPhoto : function(file) {
-
+		uploadPhoto : function(file, progress) {
+			console.log('UploadFile->', file);
+			var defer = $q.defer();
+			var i = 0;
+			(function progress() {
+				i += 10;
+				$timeout(function() {
+					if (i < 100) {
+						progress();
+					} else {
+						defer.resolve();
+					}
+				}, 200);
+			})();
+			return defer.promise;
 		}
 	};
 });
